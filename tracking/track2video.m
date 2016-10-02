e969deeea8f3c39d479bc3fcc6cadc2f35bc683f
Leo <leooooo@bu.edu>
2016-10-01 20:22:36 -0400

    got it working

--- tracking/track2video.m
+++ tracking/track2video.m
@@ -5,7 +5,8 @@
 function track2video(data, output_name, file_path, file_format, start_fr, wsize, show_track)
     
 linewidth = 2;
-mov = avifile(sprintf('%s.avi', output_name));
+mov = VideoWriter(sprintf('%s.avi', output_name));
+open(mov)
 
 for i=1:size(data, 1)
     im = double(imread(sprintf('%s%05d.%s', file_path, i+start_fr, file_format)));
@@ -16,11 +17,12 @@ for i=1:size(data, 1)
         imshow(im);
         pause(0.1);
     end;
-    mov = addframe(mov, im);
+    % mov = addframe(mov, im);
+    writeVideo(mov, im);
 
 end;
     
-mov = close(mov);
+close(mov);
     
     
     
